cf1b965dc95c24b35c29baa7a1a8b380bb8c944a
dluffy121 <waghela.sonic@gmail.com>
2022-01-04 22:41:15 +0530

    - updated OpenGLRenderer, added Log to all gl functions

--- OpenGLRenderer/src/OpenGLRenderer.cpp
+++ OpenGLRenderer/src/OpenGLRenderer.cpp
@@ -31,31 +31,31 @@ int main(void)
 	};
 
 	unsigned int bufferId;
-	glGenBuffers(1, &bufferId);															// Generate 1 Buffer and get its id in 'bufferId' https://docs.gl/gl4/glGenBuffers
-	glBindBuffer(GL_ARRAY_BUFFER, bufferId);											// Bind the generated buffer with its id https://docs.gl/gl4/glBindBuffer
-	glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), points, GL_STATIC_DRAW);		// update buffer data with its type of usage https://docs.gl/gl4/glBufferData
+	GLLog(glGenBuffers(1, &bufferId));															// Generate 1 Buffer and get its id in 'bufferId' https://docs.gl/gl4/glGenBuffers
+	GLLog(glBindBuffer(GL_ARRAY_BUFFER, bufferId));												// Bind the generated buffer with its id https://docs.gl/gl4/glBindBuffer
+	GLLog(glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), points, GL_STATIC_DRAW));		// update buffer data with its type of usage https://docs.gl/gl4/glBufferData
 
-	glEnableVertexAttribArray(0);														// enables vertex atrrib array from the bound buffer https://docs.gl/gl4/glEnableVertexAttribArray
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);				// define an array vertex attribute data
+	GLLog(glEnableVertexAttribArray(0));														// enables vertex atrrib array from the bound buffer https://docs.gl/gl4/glEnableVertexAttribArray
+	GLLog(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));				// define an array vertex attribute data
 
 	unsigned int ibo;
-	glGenBuffers(1, &ibo);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+	GLLog(glGenBuffers(1, &ibo));
+	GLLog(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+	GLLog(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW));
 
 	ShaderProgramSource source = ShaderManager->ParseShader("resources/shaders/Basic.shader");
 	unsigned int shader = ShaderManager->CreateShader(source.VertexSource, source.FragmentSource);
-	glUseProgram(shader);
+	GLLog(glUseProgram(shader));
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
-		glClear(GL_COLOR_BUFFER_BIT);
+		GLLog(glClear(GL_COLOR_BUFFER_BIT));
 
 		//OpenGLHelper->GLClearError();
-		//glDrawArrays(GL_TRIANGLES, 0, 6);												// this method will draw from binded buffer array https://docs.gl/gl4/glDrawArrays
-		GLLog(glDrawElements(GL_TRIANGLES, 6, GL_INT, nullptr));						// this method will draw from binded element buffer array https://docs.gl/gl4/glDrawElements
+		//glDrawArrays(GL_TRIANGLES, 0, 6);														// this method will draw from binded buffer array https://docs.gl/gl4/glDrawArrays
+		GLLog(glDrawElements(GL_TRIANGLES, 6, GL_INT, nullptr));								// this method will draw from binded element buffer array https://docs.gl/gl4/glDrawElements
 		//ASSERT(OpenGLHelper->GLLogCall());
 
 		/* Swap front and back buffers */
@@ -65,7 +65,7 @@ int main(void)
 		glfwPollEvents();
 	}
 
-	glDeleteProgram(shader);
+	GLLog(glDeleteProgram(shader));
 
 	OpenGLHelper->TerminateGLFW();
 	return 0;
