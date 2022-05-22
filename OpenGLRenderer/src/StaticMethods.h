994633267c4cf3e3995b1e68281f9022dc9e3ad2
dluffy121 <waghela.sonic@gmail.com>
2022-05-22 23:22:47 +0530

    - added some static method

--- OpenGLRenderer/src/StaticMethods.h
+++ OpenGLRenderer/src/StaticMethods.h
@@ -2,7 +2,19 @@
 #include <string>
 
 template <typename T>
-static std::string GetType()
+static type_info GetType()
+{
+	return typeid(T);
+}
+
+template <typename T>
+static std::string GetTypeName()
 {
 	return typeid(T).name();
+}
+
+template <typename T>
+static size_t GetTypeHash()
+{
+	return typeid(T).hash_code();
 }
