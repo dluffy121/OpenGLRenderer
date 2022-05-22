86e0e071614300444c67aaae9125b393a4d7ebf6
dluffy121 <waghela.sonic@gmail.com>
2022-05-22 23:13:10 +0530

    - fixed error while destroying local buffer if already destroyed

--- OpenGLRenderer/src/Core/GL/Texture/Texture.cpp
+++ OpenGLRenderer/src/Core/GL/Texture/Texture.cpp
@@ -7,16 +7,15 @@
 Texture::Texture(const std::string& texturePath) :
 	m_TextureId(0),
 	m_FilePath(texturePath),
-	m_LocalBuffer(nullptr),
 	m_Width(0),
 	m_Height(0),
 	m_BPP(0)
 {
 	stbi_set_flip_vertically_on_load(1);
-	m_LocalBuffer = stbi_load(m_FilePath.c_str(), &m_Width, &m_Height, &m_BPP, 0);	// 4 is for RGBA
+	unsigned char* localBuffer; = stbi_load(m_FilePath.c_str(), &m_Width, &m_Height, &m_BPP, 0);	// 4 is for RGBA
 
 	Log("Texture with id: " << m_TextureId << std::endl
-		<< "Load Status: " << (m_LocalBuffer != nullptr) << std::endl
+		<< "Load Status: " << (localBuffer != nullptr) << std::endl
 		<< "Resolution: " << m_Width << " x " << m_Height << std::endl
 		<< "Bits Per Pixel: " << m_BPP << std::endl);
 
@@ -28,20 +27,16 @@ Texture::Texture(const std::string& texturePath) :
 	GLLog(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
 	GLLog(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
 
-	GLLog(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, m_Width, m_Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, m_LocalBuffer));
+	GLLog(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, m_Width, m_Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, localBuffer));
 
 	GLLog(glBindTexture(GL_TEXTURE_2D, 0));
 
-	if (m_LocalBuffer)					// we can unload the data, since we just need to show the image and opengl already used the m_LocalBuffer to create texture
-		stbi_image_free(m_LocalBuffer);
+	if (localBuffer)					// we can unload the data, since we just need to show the image and opengl already used the m_LocalBuffer to create texture
+		stbi_image_free(localBuffer);
 }
 
 Texture::~Texture()
 {
-	// free local buffer is data was needed for other purposes like sampling
-	if (m_LocalBuffer)
-		stbi_image_free(m_LocalBuffer);
-
 	GLLog(glDeleteTextures(1, &m_TextureId));
 }
 
--- OpenGLRenderer/src/Core/GL/Texture/Texture.h
+++ OpenGLRenderer/src/Core/GL/Texture/Texture.h
@@ -6,7 +6,6 @@ class Texture
 private:
 	unsigned int m_TextureId;
 	std::string m_FilePath;
-	unsigned char* m_LocalBuffer;
 	int m_Width, m_Height, m_BPP;
 
 public:
