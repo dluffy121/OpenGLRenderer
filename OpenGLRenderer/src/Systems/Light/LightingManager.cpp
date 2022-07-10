ec7475e3af3436d65fb57b508d099894b04b01d0
dluffy121 <waghela.sonic@gmail.com>
2022-07-10 15:01:19 +0530

    - fixed UnSubscribe methods

--- OpenGLRenderer/src/Systems/Light/LightingManager.cpp
+++ OpenGLRenderer/src/Systems/Light/LightingManager.cpp
@@ -39,10 +39,12 @@ void LightingManager::Subscribe<SpotLight>(SpotLight* light)
 template<>
 void LightingManager::UnSubscribe<DirectionalLight>(DirectionalLight* light)
 {
+	bool found = false;
 	for (auto i = m_DirectionalLights.begin(); i != m_DirectionalLights.end(); i++)
 	{
 		if (*i == light)
 		{
+			found = true;
 			auto tmp = *i;
 			*i = *(m_DirectionalLights.end() - 1);
 			*(m_DirectionalLights.end() - 1) = tmp;
@@ -50,16 +52,19 @@ void LightingManager::UnSubscribe<DirectionalLight>(DirectionalLight* light)
 		}
 	}
 
-	m_DirectionalLights.erase(m_DirectionalLights.end() - 1);
+	if (found)
+		m_DirectionalLights.erase(m_DirectionalLights.end() - 1);
 }
 
 template<>
 void LightingManager::UnSubscribe<PointLight>(PointLight* light)
 {
+	bool found = false;
 	for (auto i = m_PointLights.begin(); i != m_PointLights.end(); i++)
 	{
 		if (*i == light)
 		{
+			found = true;
 			auto tmp = *i;
 			*i = *(m_PointLights.end() - 1);
 			*(m_PointLights.end() - 1) = tmp;
@@ -67,16 +72,19 @@ void LightingManager::UnSubscribe<PointLight>(PointLight* light)
 		}
 	}
 
-	m_PointLights.erase(m_PointLights.end() - 1);
+	if (found)
+		m_PointLights.erase(m_PointLights.end() - 1);
 }
 
 template<>
 void LightingManager::UnSubscribe<SpotLight>(SpotLight* light)
 {
+	bool found = false;
 	for (auto i = m_SpotLights.begin(); i != m_SpotLights.end(); i++)
 	{
 		if (*i == light)
 		{
+			found = true;
 			auto tmp = *i;
 			*i = *(m_SpotLights.end() - 1);
 			*(m_SpotLights.end() - 1) = tmp;
@@ -84,7 +92,8 @@ void LightingManager::UnSubscribe<SpotLight>(SpotLight* light)
 		}
 	}
 
-	m_SpotLights.erase(m_SpotLights.end() - 1);
+	if (found)
+		m_SpotLights.erase(m_SpotLights.end() - 1);
 }
 
 void LightingManager::UpdateShaderLightData(Shader& shader, Transform& cameraTransform, Transform& vastuTransform)
