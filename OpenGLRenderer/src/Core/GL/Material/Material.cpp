f694e6021099a396418a8d5ad01e29d3952733f5
dluffy121 <waghela.sonic@gmail.com>
2022-07-13 20:51:30 +0530

    - updated Material, added Shader support

--- OpenGLRenderer/src/Core/GL/Material/Material.cpp
+++ OpenGLRenderer/src/Core/GL/Material/Material.cpp
@@ -4,29 +4,39 @@ namespace core::gl
 {
 	Material::Material() :
 		Id(reinterpret_cast<unsigned int>(this)),
-		m_Shader(NULL),
-		m_AmbientColor(Vec3(0.0f)),
-		m_DiffuseColor(Vec3(0.0f)),
-		m_SpecularColor(Vec3(0.0f)),
-		m_DiffuseTexture(NULL),
-		m_SpecularExponent(NULL)
+		m_Shader			(NULL),
+		m_AmbientColor		(Vec3(1.0f)),
+		m_DiffuseColor		(Vec3(1.0f)),
+		m_SpecularColor		(Vec3(1.0f)),
+		m_DiffuseTexture	(NULL),
+		m_SpecularExponent	(NULL)
 	{}
 
 	Material::~Material()
 	{
-		if (m_DiffuseTexture) delete m_DiffuseTexture;
+		if (m_DiffuseTexture)	delete m_DiffuseTexture;
 		if (m_SpecularExponent) delete m_SpecularExponent;
 	}
 
 	void Material::Bind() const
 	{
-		if (m_DiffuseTexture) m_DiffuseTexture->BindToUnit(0);
+		if (m_Shader)
+		{
+			m_Shader->Bind();
+			m_Shader->SetUniform3f("u_Material.ambientColor", m_AmbientColor.x, m_AmbientColor.y, m_AmbientColor.z);
+			m_Shader->SetUniform3f("u_Material.diffuseColor", m_DiffuseColor.x, m_DiffuseColor.y, m_DiffuseColor.z);
+			m_Shader->SetUniform3f("u_Material.specularColor", m_SpecularColor.x, m_SpecularColor.y, m_SpecularColor.z);
+		}
+
+		if (m_DiffuseTexture)	m_DiffuseTexture->BindToUnit(0);
 		if (m_SpecularExponent) m_SpecularExponent->BindToUnit(1);
 	}
 
 	void Material::UnBind() const
 	{
-		if (m_DiffuseTexture) m_DiffuseTexture->UnBindFromUnit(0);
+		if (m_Shader) m_Shader->UnBind();
+
+		if (m_DiffuseTexture)	m_DiffuseTexture->UnBindFromUnit(0);
 		if (m_SpecularExponent) m_SpecularExponent->UnBindFromUnit(1);
 	}
 };
